0d9b38324545dfa34939039538b8dd23ff5198a8
nikaabula55goa <nikaabuladze1011@gmail.com>
2025-02-23 18:46:41 +0300

    second comit'

---
+++ level 66/cw/skript.js
@@ -0,0 +1,3 @@
+//  1) შექმენით ლოგიკური ოპერატორების გამოყენებით ყველა შესაძლო კომბინაცია გამოიუყენეთ and და or ლოგიკური ოპერატორები, &&, ||
+console.log(true && true, true && false, false && true, false && false);
+console.log(true || true, true || false, false || true, false || false);
